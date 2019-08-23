3dceec78eb5ce93c5ba8b5553df73b41f6c66a8b
Shweta <38096594+valkyrie55@users.noreply.github.com>
2019-08-23 08:36:28 +0100

    Update playing_audio.py

--- playing_audio.py
+++ playing_audio.py
@@ -1,5 +1,5 @@
 #Playing audio with python-sounddevice and soundfile
-import soundfile as sf
+import soundfile as sf  #for audio conversion
 import sounddevice as sd
 file = 'C:/Users/UPES/Pictures/Audio_Speech_analysis/data/kaggle_audio_analysis/audio_test/00c4d5b8.wav'
 data, fs = sf.read(file, dtype = 'float32')
