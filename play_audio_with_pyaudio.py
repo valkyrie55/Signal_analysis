927d7513d7731cfd4c2ca551f45a75ef0ea1a328
Shweta <38096594+valkyrie55@users.noreply.github.com>
2019-08-23 08:19:04 +0100

    incomplete

--- play_audio_with_pyaudio.py
+++ play_audio_with_pyaudio.py
@@ -1,3 +1,7 @@
 import pyaudio
 import wave
-file = ''
+file = 'C:/Users/UPES/Pictures/Audio_Speech_analysis/data/kaggle_audio_analysis/audio_test/00c4d5b8.wav'
+chunk = 1024
+wf = wave.open(file, 'rb')
+
+
