2985b26afc17e6ff5410395136046f0997f29845
Shweta <38096594+valkyrie55@users.noreply.github.com>
2019-08-23 07:57:06 +0100

    Update playing_audio.py

--- playing_audio.py
+++ playing_audio.py
@@ -1,1 +1,7 @@
 #Playing audio with python-sounddevice and soundfile
+import soundfile as sf
+import sounddevice as sd
+file = 'C:/Users/UPES/Pictures/Audio_Speech_analysis/data/kaggle_audio_analysis/audio_test/00c4d5b8.wav'
+data, fs = sf.read(file, dtype = 'float32')
+sd.play(data, fs)
+status = sd.wait() #wait until file is done playing
